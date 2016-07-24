420d431dd4c2bb6b7a79af28bc2605a87fe9ae6d
Gwen Dallas <jim.dallas@gmail.com>
2016-07-23 21:36:59 -0500

    turn handshake into function

--- handshake.py
+++ handshake.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python
 
+########################################
+##
+## handshake.py
+## ------------
+## Author: ad5nl@arrl.org
+## Date: July 2016
+## 
+## Purpose: This handshakes with the
+## DORJI DRA818V module using the 
+## Raspberry Pi /dev/serial0 UART 
+## interface. Connect pins 8 and 10 on
+## the Pi to pins 16 and 17 on the 
+## radio.
+##
+########################################
+
 import sys
 import serial
 
-device = 'DORJI DRA818V'
-baudrate = 9600
-handshake_request = 'AT+DMOCONNECT\r\n'
-expected_response = '+DMOCONNECT:0\r\n'
-default_serial = '/dev/serial0'
+def handshake( serial_device ):
+	device = 'DORJI DRA818V'
+	baudrate = 9600
+	handshake_request = 'AT+DMOCONNECT\r\n'
+	expected_response = '+DMOCONNECT:0\r\n'
+	default_serial = '/dev/serial0'
 
+	total = len(sys.argv)
+	cmdargs = sys.argv
 
-total = len(sys.argv)
-cmdargs = sys.argv
+	if  total > 1:
+		ttydev = str(serial_device)
+	else:
+		ttydev = default_serial
 
-if  total > 1:
-	ttydev = str(cmdargs[1])
-else:
-	ttydev = default_serial
+	print ("Attempting handshake with %s on: %s " % (device,ttydev))
 
-print ("Attempting handshake with %s on: %s " % (device,ttydev))
+	ser = serial.Serial(port=ttydev,baudrate=baudrate)
 
-ser = serial.Serial(port=ttydev,baudrate=baudrate)
+	ser.write(handshake_request)
+	response = ser.readline()
 
-ser.write(handshake_request)
-response = ser.readline()
+	if str(response) == expected_response:
+		print ("Success: %s " % str(response))
+		exit(0)
+	else:
+		print ("Fail: %s " % str(response))
+		exit(1)
 
-if str(response) == expected_response:
-	print ("Success: %s " % str(response))
-	exit(0)
-else:
-	print ("Fail: %s " % str(response))
-	exit(1)
+handshake( "/dev/serial0" )
--- init_radio.sh
+++ init_radio.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+########################################
+##
+## init_radio.sh
+## -------------
+## Author: ad5nl@arrl.net
+## Date: July 2016
+##
+## Setup: connect Dorji DRA818V module
+## to Raspberry Pi using these pins
+##
+## RASPBERRY PI       DRA818V
+## -----------------------------
+## 6 or 9 GND         10 Ground
+## 8 TXD              16 RXD
+## 10 RXD             17 TXD
+##
+## Purpose: The purpose of this is to 
+## turn off the PTT and Sleep mode on
+## the DRA818V, then to test UART by
+## running handshake.py. This Python 
+## script sends a handshake to the 
+## DRA818V.
+##
+########################################     
+
+
 if [ ! -f /sys/class/gpio/gpio17/value ]
 then
 	echo 17 > /sys/class/gpio/export
