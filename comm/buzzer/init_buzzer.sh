b801b1242aba46945119a21d828cd35f813180bb
Gwen Dallas <ad5nl@arrl.net>
2016-08-23 04:56:00 +0000

    new SD card

--- comm/buzzer/init_buzzer.sh
+++ comm/buzzer/init_buzzer.sh
@@ -2,30 +2,19 @@
 
 ########################################
 ##
-## init_radio.sh
+## init_buzzer.sh
 ## -------------
 ## Author: ad5nl@arrl.net
-## Date: July 2016
-##
-## Setup: connect Dorji DRA818V module
-## to Raspberry Pi using these pins
-##
-## RASPBERRY PI       DRA818V
-## -----------------------------
-## 6 or 9 GND         10 Ground
-## 8 TXD              16 RXD
-## 10 RXD             17 TXD
-##
-## Purpose: The purpose of this is to 
-## turn off the PTT and Sleep mode.
+## Date: August 2016
 ##
 ########################################     
 
 COMM_DIR="/home/pi/aerial/comm"
-PTT_GPIO=17
-SLP_GPIO=27
+GPIO=16
 
 source $COMM_DIR/comm_functions.sh
 
-init_gpio $PTT_GPIO out 1
-init_gpio $SLP_GPIO out 1
+init_gpio $GPIO out 0
+gpio_toggle $GPIO "sleep 1"
+
+
---
+++ comm/buzzer/long_buzzer_program.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/home/pi/aerial/comm/buzzer/init_buzzer.sh
+sleep 1
+source /home/pi/aerial/comm/comm_functions.sh
+key 16 "VVV AERIAL1 BALLOON DE AD5NL. IF FOUND PSE EMAIL AD5NL AT ARRL DOT NET THX 73S. VVV DE AD5NL VVV"
